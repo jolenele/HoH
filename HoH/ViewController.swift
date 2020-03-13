5da16aa0011b8bf7a1df4a2b1a6cb1f53f0eaad5
Jolene Le <renreine4x1@gmail.com>
2020-03-12 23:56:45 -0400

    Update ViewController.swift
    trying add new row to the table view unseccessful

--- HoH/ViewController.swift
+++ HoH/ViewController.swift
@@ -22,11 +22,29 @@ class ViewController: UIViewController {
     var image_picker = UIImagePickerController()
     var alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
     
+    
+    
     @IBAction func IBOulet_New_Post(_ sender: UIButton) {
         ImagePickerManager().pickImage(self){
             img in
             self.IBOulet_Image.image = img
+            self.IBOutlet_Image_Text.text = String("Hello")
         }
-    }
-}
+            var imgArray = [String]()
+            imgArray.append(String("Hello"))
+        let buttonPosition = sender.convert(CGPoint.zero, to: self.IBOutlet_Image_View)
+        let indexPath = self.IBOutlet_Image_View.indexPathForRow(at: buttonPosition)
+        print("indexPath \(indexPath!)")
+        
+        self.IBOutlet_Image_View.beginUpdates()
+         IBOutlet_Image_View.insertRows(at: [IndexPath(row: imgArray.count-1, section: 0)], with: .automatic)
+        self.IBOutlet_Image_View.insertRows(at: [indexPath!], with: .automatic)
+        self.IBOutlet_Image_View.endUpdates()
 
+    }
+    
+    func numberOfSections(in IBOutlet_Image_View: UITableView) -> Int {
+        return 2
+    }
+    
+}
