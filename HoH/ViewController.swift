a70753a7b2b7d9a96861e3d684c8dc3a95eb0a10
Jolene Le <renreine4x1@gmail.com>
2020-03-12 22:26:30 -0400

    Initial Commit

---
+++ HoH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HoH
+//
+//  Created by Nga Thi Phuong Le on 2020-03-12.
+//  Copyright © 2020 Jolene. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
