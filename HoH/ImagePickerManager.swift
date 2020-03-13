a0e8aba8a9d2ff4f65418a20274f47a4313b47e3
Jolene Le <renreine4x1@gmail.com>
2020-03-12 22:58:36 -0400

    Create Image Picker Manager
    Upload Image from camera roll

---
+++ HoH/ImagePickerManager.swift
@@ -0,0 +1,56 @@
+//
+//  ImagePickerManager.swift
+//  HoH
+//
+//  Created by Nga Thi Phuong Le on 2020-03-12.
+//  Copyright © 2020 Jolene. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ImagePickerManager : NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    var picker = UIImagePickerController()
+    var alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+    var viewController: UIViewController?
+    var pickerImageCallBack : ((UIImage) -> ())?
+    
+    override init() {
+        super.init()
+    }
+    
+    func pickImage(_ viewController: UIViewController, _ callback: @escaping ((UIImage) -> ())) {
+        self.viewController = viewController
+        pickerImageCallBack = callback
+        
+        let chooseImage = UIAlertAction(title: "Camera roll", style: .default){
+            UIAlertAction in self.openCameraRoll()
+        }
+        picker.delegate = self
+        alert.addAction(chooseImage)
+        alert.popoverPresentationController?.sourceView = self.viewController!.view
+        viewController.present(alert, animated: true, completion: nil)
+    }
+    
+    func openCameraRoll(){
+        alert.dismiss(animated: true, completion: nil)
+        picker.sourceType = .photoLibrary
+        self.viewController!.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let img = info[.originalImage] as? UIImage else {
+            fatalError("Expected a gallery containing images, but was provided the: \(info)" )
+        }
+        pickerImageCallBack?(img)
+    }
+    
+    @objc func imagePickerController(_ picker: UIImagePickerController, pickedImage: UIImage?) {
+    }
+}
+
--- HoH/ViewController.swift
+++ HoH/ViewController.swift
@@ -14,7 +14,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var IBOutlet_Image_View: UITableView!
+    
+    @IBOutlet weak var IBOulet_Image: UIImageView!
+    @IBOutlet weak var IBOutlet_Image_Text: UITextView!
+    
+    var image_picker = UIImagePickerController()
+    var alert = UIAlertController(title: "Choose Image", message: nil, preferredStyle: .actionSheet)
+    
+    @IBAction func IBOulet_New_Post(_ sender: UIButton) {
+        ImagePickerManager().pickImage(self){
+            img in
+            self.IBOulet_Image.image = img
+        }
+    }
 }
 
