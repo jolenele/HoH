04b1cff60584c7e462887c5ee9ce15e63b09607f
Thong Nguyen <bathong170698@gmail.com>
2020-03-14 00:53:02 -0400

    Added AppIcon, Login and SignUp Screen

---
+++ HoH/CustomDesign.swift
@@ -0,0 +1,35 @@
+//
+//  CustomDesign.swift
+//  HoH
+//
+//  Created by Macintosh HD on 2020-03-13.
+//  Copyright © 2020 Jolene. All rights reserved.
+//
+
+import UIKit
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
